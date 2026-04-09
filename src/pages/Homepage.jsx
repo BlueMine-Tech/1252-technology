489cd9fb9a124154407b935bf74bc7ace728cff7
BlueMine-Tech <blueminetechnology@gmail.com>
2026-04-09 16:44:36 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,21 @@ import HeroSection from '../components/HeroSection';
 import LifecycleAssuranceSection from '../components/LifecycleAssuranceSection';
 import WhoWereBestForSection from '../components/WhoWereBestForSection';
 import IndustriesWeSupport from './../components/IndustriesWeSupport';
+import { Helmet } from "react-helmet";
 
+function Home() {
+  return (
+    <>
+      <Helmet>
+        <link rel="canonical" href="https://www.1252technology.com/" />
+      </Helmet>
+
+      {/* your existing code */}
+    </>
+  );
+}
+
+export default Home;
 
 function HomePage() {
   return (
