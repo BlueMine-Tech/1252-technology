71fb3b3f04f5b5d70e8104a0034971f67358a3b3
BlueMine-Tech <blueminetechnology@gmail.com>
2026-04-09 17:47:28 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,6 +5,34 @@ import WhoWereBestForSection from '../components/WhoWereBestForSection';
 import IndustriesWeSupport from './../components/IndustriesWeSupport';
 import { Helmet } from "react-helmet";
 
+import { Helmet } from "react-helmet";
+
+function Home() {
+  return (
+    <>
+      <Helmet>
+        <title>Best Logo & Web Design Services in Tamil Nadu | 1252 Technology</title>
+
+        <meta
+          name="description"
+          content="1252 Technology offers professional logo design, branding, and web design services in Tamil Nadu. Build your brand with us."
+        />
+
+        <meta
+          name="keywords"
+          content="logo design, web design, branding, graphic designer tamil nadu, business branding india"
+        />
+
+        <link rel="canonical" href="https://www.1252technology.com/" />
+      </Helmet>
+
+      {/* your existing code */}
+    </>
+  );
+}
+
+export default Home;
+
 function Home() {
   return (
     <>
